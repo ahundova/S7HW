3b51f80cd37c8d912659e95399967d2842c937b8
ahundova <hayat164@yahoo.com>
2024-04-02 19:13:04 +0400

    first commit

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 2: Задача 1: Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+//Использовать рекурсию, не использовать циклы.
+
+//M = 8 
+//N = 2-> "8, 7, 6, 5, 4, 3, 2"
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+NaturalToLow(m, n);
+
+
+void NaturalToLow(int m, int n)
+{
+    if (n > m)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(m, n + 1);
+        Console.Write(n + " ");
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Задача 3: Задайте произвольный массив. Выведете его элементы, 
+начиная с конца. Использовать рекурсию, не использовать циклы.*/
+Console.Clear();
+void PrintArr(int[] array, int i = 0)
+{
+    if (i != array.Length)
+    {
+        PrintArr(array, i + 1);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+var array = new int[] { 14, 31, 27, 12, 54 };
+PrintArr(array);
+
